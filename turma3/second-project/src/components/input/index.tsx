6d4b68c91010ce9f48510cd18062eff150800c7a
Paulo Junior <npaulo.ce@gmail.com>
2024-03-20 22:01:19 -0300

    Aula REACT e TYPE ambiente de login 20/03

--- turma3/second-project/src/components/input/index.tsx
+++ turma3/second-project/src/components/input/index.tsx
@@ -1,13 +1,15 @@
 import { InputProps } from "../../interfaces/InputInterface"
 
-export function Input({ type, placeholder, name }: InputProps) {
+export function Input({ type, placeholder, name, register, error, rules }: InputProps) {
   return (
     <div>
         <input 
             type={type}
             placeholder={placeholder}
-            name={name} 
+            {...register(name, rules)}
+            id={name}
         />
+        { error && <p>{ error }</p> }
     </div>
   )
 }
--- turma3/second-project/src/pages/register/index.tsx
+++ turma3/second-project/src/pages/register/index.tsx
@@ -1,7 +1,67 @@
+import { Container } from "../../components/container";
+import ImgLogo from '../../assets/logo.svg'
+import { Link } from "react-router-dom";
+import { Input } from "../../components/input";
+
+import { useForm } from "react-hook-form";
+import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string(),
+  email: z.string().email("Insira um e-mail válido"),
+  password: z.string()
+})
+
+type FormData = z.infer<typeof schema>
+
 export function Register() {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: 'onChange'
+  })
+
+  function onSubmit(data: FormData){
+    console.log(data)
+  }
+
   return (
-    <div>
-        <h2>Página de Registro</h2>
-    </div>
+    <Container>
+      <div>
+        <Link to='/'>
+          <img src={ ImgLogo } alt="Logo" />
+        </Link>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+            <Input
+              type="text"
+              placeholder="Digite seu nome..."
+              name="name"
+              error={errors.name?.message}
+              register={register}
+            />
+          </div>
+          <div>
+            <Input
+              type="email"
+              placeholder="Digite seu e-mail..."
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+          <div>
+            <Input
+              type="password"
+              placeholder="Digite sua senha..."
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+          <button>Cadastrar</button>
+        </form>
+      </div>
+    </Container>
   )
 }
